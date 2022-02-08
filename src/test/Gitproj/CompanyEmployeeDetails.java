e4354f24028e6b9e0b3458ac9358e75a4d1e89f9
irfana9 <irfana.s0992@gmail.com>
2022-02-08 21:01:37 +0530

    sample code task

---
+++ src/test/Gitproj/CompanyEmployeeDetails.java
@@ -0,0 +1,18 @@
+package test.Gitproj;
+
+public class CompanyEmployeeDetails {
+          public void empName() {
+			System.out.println("ryan");
+
+		}
+          public void empId() {
+			System.out.println("empID is....1245");
+
+		}
+	public static void main(String[] args) {
+		CompanyEmployeeDetails ce = new CompanyEmployeeDetails();
+		ce.empName();
+        ce.empId();
+	}
+
+}
